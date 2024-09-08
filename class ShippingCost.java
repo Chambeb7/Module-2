890c78759e354c9034640ed6ff2bcec5ed162fc1
Brady <chambeb7@mail.uc.edu>
2024-09-08 19:19:50 -0400

    Add files via upload

---
+++ class ShippingCost.java
@@ -0,0 +1,19 @@
+class ShippingCost
+	(main)
+		num itemPrice = $0
+		num costOfShipping = $0
+		output “What is the Price of the item”
+		input itemPrice
+		if itemPrice => $100 
+		then
+		output costOfShipping = $0 
+		else	
+		process// itemPrice * 0.02
+		output  
+		
+		
+	return
+end class 
+
+
+
